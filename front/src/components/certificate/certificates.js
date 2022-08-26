057c5d4804024a43b8c9d499bccbaf91725cfe90
eunryeong1 <meufavorr@gmail.com>
2022-08-26 16:45:35 +0900

    certificate finish

---
+++ front/src/components/certificate/certificates.js
@@ -0,0 +1,58 @@
+import { Card, Button, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import CertifiAddForm from "./certiAddForm";
+import Certificate from "./certificate";
+import * as Api from "../../api";
+
+function Certificates({ portfolioOwnerId, isEditable }) {
+  const [isAdding, setIsAdding] = useState(false)
+  const [certificates, setCertificates] = useState([])
+
+
+  useEffect(() => {
+    // "`users/${portfolioOwnerId}/certificate`"로 GET
+    Api.get(`users/${portfolioOwnerId}/certi`).then((res) => setCertificates(res.data));
+  }, [portfolioOwnerId]);
+
+  return (
+    <>
+    <Card>
+      <Card.Body>
+      <Card.Title>자격증</Card.Title>
+          {certificates.map((certificate)=>(
+            <Certificate
+            key={certificate.id}
+            certificate={certificate}
+            setCertificates={setCertificates}
+            isEditable={isEditable}
+            />
+          ))}
+
+        
+      {isAdding && (
+        <CertifiAddForm
+          portfolioOwnerId={portfolioOwnerId}
+          setIsAdding={setIsAdding}
+          setCertificates={setCertificates}
+        />
+      )} 
+      {isEditable&&(
+        <Row className="mt-3 text-center text-info">
+        <Col sm={{ span: 20 }}>
+          <Button
+            variant="outline-info"
+            size="sm"
+            onClick={     () => setIsAdding(true)       }
+          >
+            +
+          </Button>
+        </Col>
+      </Row>
+      )}
+      </Card.Body>
+    </Card>
+    </>
+  );
+}
+
+export default Certificates;
